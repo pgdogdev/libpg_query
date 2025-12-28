f653ab903089bd3cd9c2419315cdc961678a0a8e
Lukas Fittl <lukas@fittl.com>
2025-12-28 09:07:58 -0800

    Deparser: Rework comment handling for PG 18 multi-statement strings
    Previously a subsequent statement in a multi-statement string would start
    at the end of the prior statement, including any comments between. In
    Postgres 18 this has changed, and such comments (and whitespace) are
    excluded from the statements, the next statement starts at the first
    non-whitespace (and non-comment) character.
    
    Because of this we change the comments logic in the deparser: Previously
    comments that had a location outside of the statement would be ignored
    by deparseRawStmtOpts, which processes a single RawStmt. Instead, ensure
    that all comments passed by the caller are emitted. This is a backwards
    incompatible change for any direct callers of the deparser code.
    
    For callers that call libpg_query instead of the deparser directly, e.g.
    by using pg_query_deparse_protobuf_opts, nothing changes, as comments
    are now split up before passing them down to the deparser code.

--- src/postgres_deparse.c
+++ src/postgres_deparse.c
@@ -3048,16 +3048,6 @@ void deparseRawStmtOpts(StringInfo str, struct RawStmt *raw_stmt, PostgresDepars
 	/* Check for any comments at the start of the statement */
 	if (state->opts->comment_count > 0)
 	{
-		/*
-		 * Filter out comments that are placed before this statement starts, this
-		 * avoids emitting comments multiple times in multi-statement queries.
-		 */
-		for (int i = 0; i < state->opts->comment_count; i++)
-		{
-			if (state->opts->comments[i]->match_location < raw_stmt->stmt_location)
-				state->emitted_comments = bms_add_member(state->emitted_comments, i);
-		}
-
 		deparseStateIncreaseNestingLevel(state);
 		deparseAppendCommentsIfNeeded(state, raw_stmt->stmt_location);
 		deparseRemoveTrailingEmptyPart(state);
@@ -3065,6 +3055,15 @@ void deparseRawStmtOpts(StringInfo str, struct RawStmt *raw_stmt, PostgresDepars
 	}
 
 	deparseStmt(state, raw_stmt->stmt);
+
+	/* Check for any comments at the end of the statement */
+	if (state->opts->comment_count > 0)
+	{
+		deparseStateIncreaseNestingLevel(state);
+		deparseAppendCommentsIfNeeded(state, INT_MAX);
+		deparseRemoveTrailingEmptyPart(state);
+		deparseStateDecreaseNestingLevel(state, NULL);
+	}
 
 	deparseEmit(state, str);
 
